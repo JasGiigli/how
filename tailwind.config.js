dd578b3cd0d51caaf1f139e87288692e494e5e96
JasGiigli <overview.jjj@gmail.com>
2024-01-02 23:09:45 +0500

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        DarkYellow: '#fa9702',
+        DarkPurple: '#9702fa',
+      },
+    },
+  },
+  plugins: [],
+}
+
